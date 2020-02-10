7d79bd6aebbe0233b8fbe0815a0b8a86a8e909af
ClaireLuo <30530100+ClaireLuo@users.noreply.github.com>
2020-02-10 18:54:20 -0500

    Added upload(pathname)
    upload(pathname) now support reading file from given path (pathname), and returns parsed data in anndata fromat. 
    To view gene expression matrix use "data.X", to view cell information use "data.obs", to view gene information use "data.var".
    The function identifies the file format and supports mtx, txt, csv, xlsx,etc that are supported by Scanpy and added support for npz and mat. npz & mat save files like dictionaries, the element with largest size will be saved as "X", elements with same size as cell count will be saved into "obs", elements with same size as gene count will be saved into "var".

--- helper_functions.py
+++ helper_functions.py
@@ -1,13 +1,89 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[36]:
+# In[147]:
 
 
-def upload(filename):
-    print(filename, " uploaded !")
-    #TODO
-    return
+def upload(pathname):
+    import scanpy as sc
+    import os
+    import anndata
+    from scipy.sparse import csr_matrix
+    filename, file_extension = os.path.splitext(pathname)
+    if file_extension == ".mat":
+        from scipy.io import loadmat
+        import pandas as pd
+        x = loadmat(pathname)
+        keys = []
+        for key in x.keys():
+            keys.append(key)
+
+        #obs is the cell
+        #var is gene
+        #pick the largest
+        largest = 3
+        largest_size = 0
+        for i in range(len(keys) - 3):
+            if len(x[keys[i+3]].shape) == 2 :
+                size = ( x[keys[i+3]].shape[0] * x[keys[i+3]].shape[1] )
+            else:
+                size = x[keys[i+3]].shape[0]
+            if size >= largest_size:
+                largest = i+3
+                largest_size = size
+        obs_d,var_d = {},{}
+        for i in range(len(keys) - 3):
+            if i != largest-3:
+                if (x[keys[i+3]].flatten() ).shape[0] == (x[keys[largest]]).shape[0]:
+                    obs_d[keys[i+3]] = x[keys[i+3]].flatten()
+                elif (x[keys[i+3]].flatten()).shape[0] == (x[keys[largest]]).shape[1]:
+                    var_d[keys[i+3]] = x[keys[i+3]].flatten()
+                #else:
+        obs_df = pd.DataFrame(data=obs_d)
+        var_df = pd.DataFrame(data=var_d)
+        
+        data = anndata.AnnData(X=x[keys[largest]],obs=None if obs_df.empty else obs_df,var=None if var_df.empty else var_df)
+        
+    elif file_extension == ".npz":
+        import numpy as np 
+        import pandas as pd
+        x = np.load(pathname)
+        #pick largest size file
+        largest = 0
+        largest_size = 0
+        for i in range(len(x.files)):
+            if len(x[x.files[i]].shape) == 2 :
+                size = ( x[x.files[i]].shape[0] * x[x.files[i]].shape[1] )
+            else:
+                size = x[x.files[i]].shape[0]
+            if size >= largest_size :
+                largest = i
+                largest_size = size
+        obs_d,var_d = {},{}        
+        for i in range(len(x.files)):
+            if i != largest:
+                if len(x[x.files[i]].flatten() ) == len(x[x.files[largest]]):
+                    obs_d[x.files[i]] = x[x.files[i]].flatten()
+                elif len(x[x.files[i]].flatten() ) == len(x[x.files[largest]][0]):
+                    var_d[x.files[i]] = x[x.files[i]].flatten()
+                #else:
+        obs_df = pd.DataFrame(data=obs_d)
+        var_df = pd.DataFrame(data=var_d)
+        data = anndata.AnnData(X=x[x.files[largest]],obs=None if obs_df.empty else obs_df,var=None if var_df.empty else var_df)
+    elif file_extension == ".mtx":
+        data = sc.read_10x_mtx(os.path.dirname(pathname))
+    elif file_extension == ".csv":
+        data = sc.read_csv(pathname)
+    elif file_extension == ".xlsx":
+        data = sc.read_excel(pathname)
+    elif file_extension == ".txt":
+        data = sc.read_text(pathname)
+    else:
+        data = sc.read(pathname)
+    
+    
+    print(pathname, " uploaded !")
+    return data
 
 def download(email):
     print("link sent to ", email)
@@ -125,10 +201,47 @@ with open('test.csv', newline='') as csvfile:
 parse_request(data)
 
 
-# In[42]:
+# In[166]:
 
 
-print(data)
+#filename = "/Users/yumengluo/Desktop/claire/research/Test_5_Zeisel.mat"
+#filename = '/Users/yumengluo/Desktop/claire/research/cortex.npz'
+filename = '/Users/yumengluo/Desktop/claire/BME498/data/filtered_gene_bc_matrices/hg19/matrix.mtx'
+
+data = upload(filename)
+print(data.X)
+print(data.obs)
+print(data.var)
+
+
+# In[167]:
+
+
+data.obs
+
+
+# In[168]:
+
+
+data.var
+
+
+# In[169]:
+
+
+data.X
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
