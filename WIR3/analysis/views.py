292fe23ae299757c2f86f27d5427f4268602c6e4
Rex <rexma1102@gmail.com>
2020-02-27 15:40:17 -0500

    add the whole project for WIR3

---
+++ WIR3/analysis/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+# Assume all the input here is a scanpy anndata object
+
+def clustering_seurat_0.6():
+    pass
+
+def clustering_seurat_Pipe():
+    pass
+
+def clustering_seurat_RaceID3():
+    pass
+
+def diffExpression_Linnorm():
+    pass
+
+
+def cellAnnotation_scMerge_s():
+    pass
+
+def trajectInference_Slingshot():
+    pass
+
